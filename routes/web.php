29e9ce38b97aaded50d262ecd2e51b03942b2824
unknown <sigmaligma@skibidi.com>
2024-09-17 13:36:34 +0300

    routes added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', function () {return view('welcome');});
+
+//Route::get('/employee', [NoteController::class, 'index'])->name('employee.index');
+//Route::get('/employee/create', [NoteController::class, 'cerate'])->name('employee.create');
+//Route::post('/employee', [NoteController::class, 'store'])->name('employee.store');
+//Route::get('/employee/{id}', [NoteController::class, 'show'])->name('employee.show');
+//Route::get('/employee/{id}/edit', [NoteController::class, 'edit'])->name('employee.');
+
+Route::resource('employees', EmployeeController::class);
+
+
+
